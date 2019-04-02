ba2c60a8a0a98862785af20d8053d15392c390b4
thomasbeecham <48462896+thomasbeecham@users.noreply.github.com>
2019-04-02 11:42:32 +1100

    Update Project_1.c
    Added comments

--- Project_1.c
+++ Project_1.c
@@ -1,5 +1,7 @@
 # include <stdio.h>
-//,klmklmkl
+// Note: 65 - 90 = Captial letters a-z
+// 97 - 122 = lower case letters a-z
+// (c = c - 65) converts each letter of alphabet to a number
 int main()  {
-
+  
 }
