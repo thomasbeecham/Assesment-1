699693455b434dd261769f1227d806211af2ba4e
Thomas Beecham <thomas.beecham@uon.edu.au>
2019-04-02 02:33:38 +0000

    End of lab 6 progress

--- Project_1.c
+++ Project_1.c
@@ -2,7 +2,21 @@
 // Note: 65 - 90 = Captial letters a-z
 // 97 - 122 = lower case letters a-z
 // (c = c - 65) converts each letter of alphabet to a number
-int main()  {
-  printf("Hello world!");
+void encyrptRotate(int x)
+void decryptRotate(int x)
+void encyrptSubstitute(int x)
+
+int main () {
+    
+    	char strAlpha[1024] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // initialises string
+	printf("Enter a message to encrypt: ");
+	scanf("%d", &message);
+	
 }
+
+
+	
+	
+
+
 
