35e471fe8b8a0f8bcd01e4328fc2183828fd18a1
thomasbeecham <48462896+thomasbeecham@users.noreply.github.com>
2019-04-28 17:53:55 +1000

    Update Project_1.c
    Added substitution decryption function & comments. Comments need evaluation etc.

--- Project_1.c
+++ Project_1.c
@@ -1,33 +1,33 @@
  #include <stdio.h>
 /* 
-This c program is a basic code which prints a menu displaying the types of cyphers you can
-use as well as a choice of decrypting or encrypting
+This c program is a basic code which prints a menu displaying the types of ciphers you can
+use as well as a choice of decrypting or encrypting. 
 
 */
-// Note: 65 - 90 = Captial letters a-z
-// 97 - 122 = lower case letters a-z
+// Note:ASCII values 65 - 90 = Captial letters a-z
+//ASCII values 97 - 122 = lower case letters a-z
 
 // detting up function definitions BEFORE main
 // these functions are called individually depending on which task is wanted
 // each function performs a specific task (name indicative of task)
-char encryptRotate(unsigned char* text, int key);
-char decryptRotate(unsigned char* text, int key);
-char encryptSubstitute(unsigned char* text, int key);
-//char decryptSubstitute()
+char encryptRotate(char* text, int key);
+char decryptRotate(char* text, int key);
+char encryptSubstitute(char* text, int key);
+char decryptSubstitute(char* text, int key);
 //char decryptRotateText()
 //char decryptSubstituteText()
 
 int main () {
     unsigned char text[100];     
-    // each array is defined within main
+    //array is defined within main
     //so it doesnt have to be defined within each function.
     
     //char alphabet[100];
     //char rotateAlphabet[100];
     
     int key;
-    // printing a simple menu for choosing which task to use
-    printf("Please select an option: \n");
+    // printing a simple menu for the user to choose which task to use
+    printf("Select an option by entering the corresponding number then pressing ENTER: \n");
     printf("1) Encrypt using rotation cypher \n");
     printf("2) Decrypt using rotation cypher \n");
     printf("3) Encrypt using substitution cypher \n");
@@ -43,13 +43,13 @@ int main () {
         case 1: encryptRotate(text, key); break;
         case 2: decryptRotate(text, key); break;
         case 3: encryptSubstitute(text, key); break;
-//        case 4: decryptSubstitute(text, key); break;
+        case 4: decryptSubstitute(text, key); break;
     //  case 5: DecryptRotateText(); break;
     //  case 6: DecryptSubstituteText(); break; 
-        default: printf("Uknown option %d\n Please enter 1, 2, 3 or 4", selection); // added to increase performance of menu
+        default: printf("Uknown option %d\n Please enter 1, 2, 3 or 4\n", selection); // added to increase performance of menu
         // will print this message if the user selects a number outside of the selection (1-4)
     }
-    return 0;
+
     }	
 	
 
@@ -63,11 +63,16 @@ int main () {
  
  */
 
-char encryptRotate(unsigned char* text, int key)    {
-	unsigned char msg;
+char encryptRotate(char* text, int key) 
+{
+	unsigned char msg;     /* needs to be unsigned char as  has data values signed char from -128 to 127 
+	and unsigned char from 0 to 255.
+	*/
 	int n;
 	printf("Enter a message to encrypt: ");
-	scanf(" %[^\n]*c", text); // stores whole message entered as a char
+	scanf(" %[^\n]*c", text); // stores whole message entered as a char. 
+	//NOTE: had great trouble with this scanf as the code would skip the section.
+	//Adding a space before the % rectified this problem and allowed us to enter the message
 
 	printf("Enter Key: ");
 	scanf("%d", &key); // stores key for use in cypher 
@@ -91,18 +96,25 @@ char encryptRotate(unsigned char* text, int key)    {
 	    
 	    if (msg > 'Z')   {
 	       msg = msg - 'Z' + 'A' - 1; 
+// Performs the same task as in line 84. Just allows for encrypting of capital letter input
+// as well as lower case making the program more practical.
 	    }
 	    text[n] = msg;
 	}
     
 }
-    printf("Encrypted message is: %s\n", text);
+    printf("Encrypted message is: %s\n", text); 
+        printf("End of program\n");
     
     return 0;
 }
 /*=====================================Decrypt Rotate Function=================================*/
-
-char decryptRotate(unsigned char* text, int key)    {
+/*
+ This function is essentially the same as the encrypting function above in terms of coding 
+ and equations apart from having the OPPOSITE signs. E.g a + sign becomes a - sign, etc.
+ I have left comments for this function as it is similar to the one above.
+ */
+char decryptRotate(char* text, int key)    {
 	unsigned char msg;
 	int n;
 	printf("Enter a message to decrypt: ");
@@ -132,23 +144,25 @@ char decryptRotate(unsigned char* text, int key)    {
     
 }
 printf("Decrypted message is: %s\n", text);
+    printf("End of program\n");
 
 return 0;
 }
 
 //=================================Substitution Encryption Function==============================
 
-char encryptSubstitute(unsigned char* text, int key)
+char encryptSubstitute(char* text, int key)
 {
 
     char alpha[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q', 'R','S','T','U','V','W','X','Y','Z'};
     char newAlpha[26];
     char character;
-    int i, n;
+    int i, n;   // i stands for index in this case
     printf("Enter a message to encrypt: ");
-    scanf(" %[^\n]*c", text);
-    printf("Enter your substituted alphabet IN CAPITALS: ");
-    scanf("%s", newAlpha);
+    scanf(" %[^\n]*c", text);   
+    printf("Enter your substituted alphabet IN CAPITALS: ");    // this function requires caps input
+    //
+    scanf("%s", newAlpha);  // stores this new alphabet 
     
     for(i = 0; text[i] != '\0'; i++) {
         character = text[i]; 
@@ -172,8 +186,41 @@ char encryptSubstitute(unsigned char* text, int key)
 
 
 //=================================Substitution Decryption Function==============================
+char decryptSubstitute(char* text, int key)
+{
+    char alpha[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q', 'R','S','T','U','V','W','X','Y','Z'};
+    char newAlpha[26];
+    char character;
+    int i, n;
+    printf("Enter a message to decrypt: ");
+    scanf(" %[^\n]*c", text);
+    printf("Enter the substituted alphabet IN CAPITALS: ");
+    scanf("%s", newAlpha);
+    
+    for(i = 0; text[i] != '\0'; i++) {
+        character = text[i]; 
+        
+        if(character >= 'a' && character <= 'z')    {
+            character = character - 32; //
+        }
+    
+    for (n = 0; n <= 25; n++)   {
+        if (character == newAlpha[n])  {
+            character = alpha[n];
+            text[i] = character;
+            break;
+        }
+    }
+}
+    printf("Decrypted message is: %s\n", text);
+    printf("End of program\n");
+
+    return 0;
+}
+
 
 /*
 Decryption of a previously unseen cipher text encrypted with a rotation cipher: 1.5 marks
 Decryption of a day-1 provided block of cipher text encrypted with a substitution cipher: 1 mark
  */
+
